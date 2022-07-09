8db9a10955cb488b22592949f5f411beb7ee0184
AliMasykur10 <alimasykur2001@gmail.com>
2022-07-09 08:11:00 +0700

    menambahkan judul

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          mencoba react <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>mencoba react pada h1</h1>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>aku sedang mencoba membuat sebuah paragraph di ract js</p>
+
+    //     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
